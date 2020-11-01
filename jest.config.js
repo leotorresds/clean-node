7f3abd2ddfb41552040cdfe4329c82138122a3e9
Leonardo Torres <leonardo.torres@synvia.com>
2020-11-01 19:36:54 -0300

    feat: ensure EmailVAlidatorAdapter return true if validator returns true

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,9 @@
-
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**',
-    '!<rootDir>/src/**/*-protocols.ts',
-    '!**/protocols/**',
-    '!**/test/**'
-  ],
+  roots: ['<rootDir>/src/'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**'],
+  collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
-  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
   }
 }
