1796ceb014fcfdc2bd71d0c65362e9ee22db6025
Leonardo Torres <leonardo.torres@synvia.com>
2020-11-01 19:31:53 -0300

    feat: implement email validator

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,20 @@
+
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
